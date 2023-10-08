461fb012d624585a03e5905dd461e97f7f316c34
Sofia Llàcer Caro <sofiallacercaro@gmail.com>
2023-10-08 14:34:51 +0200

    Changes to make it public later

---
+++ Ex1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    unsigned nedges;
+    unsigned edges[8]; // limit of edges in a node = 8
+}node;
+
+int main(int argc,char *argv[]){
+    FILE * defgraph;
+    node * nodelist;
+    unsigned i,j;
+    unsigned gsize,gorder,or,dest;
+    
+    defgraph=fopen(argv[1],"r");
+    if(defgraph==NULL){
+        printf("\nERROR: Data file not found.\n");
+        return -1;
+    }    
+    fscanf(defgraph,"%u",&gorder);
+    fscanf(defgraph,"%u",&gsize);
+    if ((nodelist = (node *) malloc(gorder*sizeof(node))) == NULL){
+        fprintf(stderr, "\nERROR: no enought memory for allocating the nodes\n\n");
+        return 2;
+    }
+    printf("%s defines a graph with %d nodes and %d edges.\n",argv[1],gorder,gsize);
+    for (i=0; i<gorder; i++) nodelist[i].nedges = 0;
+    for (j=0; j<gsize; j++){
+        fscanf(defgraph,"%u %u",&or,&dest);
+        nodelist[or].edges[nodelist[or].nedges]=dest;
+        nodelist[or].nedges++;
+        nodelist[dest].edges[nodelist[dest].nedges]=or;
+        nodelist[dest].nedges++;       
+    }    
+    fclose(defgraph);
+    for (i=0; i<gorder; i++){
+        printf("Node %u has %u edges:\n",i,nodelist[i].nedges);
+        for (j=0; j<nodelist[i].nedges;j++){
+            printf("  %u --> %u\n",i,nodelist[i].edges[j]);
+        }
+    }    
+    return 0;
+
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+This repository contains the code written for one of the assignments of the course Optimisation as part of the Modelling for Science and Engineering MSc at the Universitat Autònoma de Barcelona (academic year 2023 - 2024). Note that the code in Ex0.c was kindly provided by the professor as a basis to build our own.
+
+The aim of the assignment was to examine different properties of a given graph (in the context of graph theory).
+
+In order to use this programme, it needs to be executed on a .txt file containing a sample graph. The graph's information should be formatted as is exemplified in the file 'SampleGraph.txt', which corresponds to the following diagram:
+
+[comment]: <> (Here goes the diagram of the graph.)
